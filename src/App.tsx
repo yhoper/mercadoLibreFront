c98b132046fdd82c79353aeda4fc20e69c3eaebb
Moises Perez <moisevicentepere@gmail.com>
2022-11-27 01:51:42 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './scss/App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        test
+      </header>
+    </div>
+  );
+}
+
+export default App;
