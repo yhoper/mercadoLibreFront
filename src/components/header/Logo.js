f5be4c39b32e98599d6bb858e404ee27035e3347
yhoper <moisevicenteperez@gmail.com>
2022-11-29 13:27:01 -0300

    hiperlink on logo

--- src/components/header/Logo.js
+++ src/components/header/Logo.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import logo from "../../assets/logo__large_plus@2x.png";
+
 export const Logo = () => {
   return (
     <div className="card wh-10">
-      <img className="logo" src={logo} />
+      <Link to="/">
+        <img className="logo" src={logo} />
+      </Link>
     </div>
   );
 };
